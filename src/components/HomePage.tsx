39ab72ba38dc5dedbf75c5f8065ff8cba1d66e96
mhamzaaamir960 <mhamzaaamir960.gmail.com>
2024-01-08 21:20:22 +0500

    Added files

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <main className="flex justify-center w-full min-h-screen bg-primary">
+
+      {/* sidebar */}
+      <div>
+
+      </div>
+       
+       {/* tasksInput */}
+      <div>
+        
+      </div>
+    </main>
+  );
+}
+
+export default HomePage;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backgroundColor: {
+           primary: "#F9F9F9",
+           secondary: "#4A5568"
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
